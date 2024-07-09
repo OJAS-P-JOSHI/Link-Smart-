2bc5de9bf1690b13febc3e74cc9cc08288b70bf9
OJAS-P-JOSHI <studyfor4yrs@gmail.com>
2024-07-09 21:39:18 +0530

    ORM setup

---
+++ LinkSmart/LinkSmart/src/main/java/com/LinkSmart/LinkSmartApplication.java
@@ -0,0 +1,14 @@
+package com.LinkSmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkSmartApplication.class, args);
+		
+	}
+
+}
---
+++ LinkSmart/LinkSmart/src/test/java/com/LinkSmart/LinkSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LinkSmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
